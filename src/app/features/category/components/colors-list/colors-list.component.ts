cf131e05dc8041a916abd9f0d0a4d43d4314d0e6
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-03-12 18:17:34 -0400

    feat: categories with colors

--- src/app/features/category/components/colors-list/colors-list.component.ts
+++ src/app/features/category/components/colors-list/colors-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { categoryBackgroundColors } from './../../constants/category-colors';
+import { Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-colors-list',
@@ -8,4 +10,10 @@ import { MatDividerModule } from '@angular/material/divider';
   templateUrl: './colors-list.component.html',
   styles: ``,
 })
-export class ColorsListComponent {}
+export class ColorsListComponent {
+  private readonly categoryService = inject(CategoryService);
+
+  public categories = this.categoryService.categories;
+
+  public categoryBackgroundColors = categoryBackgroundColors;
+}
