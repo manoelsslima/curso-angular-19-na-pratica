2295c0b11d49c6814059d6bc12f74fcc434dd385
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-03-12 15:35:15 -0400

    feat: components main, category and task

---
+++ src/app/layout/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CategoryComponent } from '../../features/category/view/category/category.component';
+import { TaskComponent } from '../../features/task/view/task/task.component';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CategoryComponent, MatDividerModule, TaskComponent],
+  templateUrl: './main.component.html',
+  styles: ``,
+})
+export class MainComponent {}
